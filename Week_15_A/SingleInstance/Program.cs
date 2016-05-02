87f7cc71373d58ddcf5b5270f1b23fa9a468d285
szmate <larde15@gmail.com>
2016-05-02 20:56:26 +0200

    threadPool and threading solution

---
+++ Week_15_A/SingleInstance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex oneMutex = null;
+            const string MutexName = "RUNMEONLYONCE";
+
+            try
+            {
+                oneMutex = Mutex.OpenExisting(MutexName);
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+            }
+
+            if (oneMutex == null)
+            {
+                oneMutex = new Mutex(true, MutexName);
+            }
+            else
+            {
+                oneMutex.Close();
+                return;
+            }
+
+            Console.WriteLine("Our Application");
+            Console.Read();
+        }
+    }
+}
