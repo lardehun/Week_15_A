681e13753b884d46e0bfb38b3682a15c8c69351e
szmate <larde15@gmail.com>
2016-05-02 20:50:28 +0200

    serialization solution

---
+++ Week_15_A/Serialization/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    class Person : IDeserializationCallback
+    {
+        private string name { get; set; }
+        private int birthYear { get; set; }
+        [NonSerialized]
+        private int age;
+
+        public void OnDeserialization(object sender)
+        {
+        }
+
+        public Person()
+        {
+
+        }
+
+        public Person(String name, int birthYear, int age)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + name + " Age: " + age;
+        }
+
+    }
+}
---
+++ Week_15_A/Serialization/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static void Serialize(Person sp)
+        {
+            FileStream fs = new FileStream("Person.Dat", FileMode.Create);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(fs, sp);
+
+            fs.Close();
+        }
+
+        private static Person Deserialize()
+        {
+            Person dsp = new Person();
+
+            FileStream fs = new FileStream("Person.Dat", FileMode.Open);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            dsp = (Person)bf.Deserialize(fs);
+
+            fs.Close();
+
+            return dsp;
+        }
+    }
+}
